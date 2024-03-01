930057d9b9cd09722a4f85bfc3f244edef62a7f7
Alain DS <46058614+dasc345@users.noreply.github.com>
2024-03-01 18:50:56 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+function VERT () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 1023)
+}
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "vert") {
+        ORANGE()
+    }
+    basic.pause(2000)
+    VERT()
+    radio.sendString("GO")
+    basic.pause(5000)
+    ORANGE()
+    basic.pause(2000)
+    ROUGE()
+})
+function ROUGE () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+}
+function ORANGE () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P2, 0)
+}
+radio.setGroup(1)
+ROUGE()
